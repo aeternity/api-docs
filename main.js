ae5c9dc2a25af3ab98f44e433b2afe1a68d5efc8
Dincho Todorov <dincho@users.noreply.github.com>
2022-08-30 18:54:01 +0300

    Use API gateway URLs instead of release versions

--- main.js
+++ main.js
@@ -1,46 +1,4 @@
-
-function swaggerUrl(version) {
-  //backward compatibility
-  const oldPathVersions = ['v4.1.0', 'v4.2.0', 'v4.2.1', 'v5.0.0-rc.1'];
-  if (oldPathVersions.includes(version)) {
-    return `https://raw.githubusercontent.com/aeternity/aeternity/${version}/config/swagger.yaml`;
-  }
-
-  return `https://raw.githubusercontent.com/aeternity/aeternity/${version}/apps/aehttp/priv/swagger.yaml`;
-}
-
-async function getLatestRelease() {
-    return fetch('https://api.github.com/repos/aeternity/aeternity/releases/latest')
-      .then(response => response.json())
-      .then(data => {
-        return data.tag_name;
-      })
-      .catch(error => console.error(error));
-}
-
-async function getRecentReleases() {
-    return fetch('https://api.github.com/repos/aeternity/aeternity/releases?per_page=5')
-      .then(response => response.json())
-      .then(data => {
-        return data.map(o => o['tag_name']);
-      })
-      .catch(error => console.error(error));
-}
-
-async function getSwaggerUrls() {
-    const releases = await getRecentReleases();
-
-    releases.push('master');
-
-    return releases.map(v => {
-        return {
-            name: v,
-            url: swaggerUrl(v)
-        };
-    });
-}
-
-async function load() {
+(() => {
     const params = (new URL(location)).searchParams;
     const configUrl = params.has('config') ? params.get('config') : null;
     const config = {
@@ -57,13 +15,11 @@ async function load() {
     // If the configuration is not provided as URL parameter, overwrite the url option with urls
     // https://swagger.io/docs/open-source-tools/swagger-ui/usage/configuration/
     if (!configUrl) {
-        let [urls, latest] = await Promise.all([getSwaggerUrls(), getLatestRelease()]);
-        config.urls = urls;
-        config['urls.primaryName'] = latest;
+        config.urls = [
+            {name: 'mainnet', url: 'https://mainnet.aeternity.io/api?oas3'},
+            {name: 'testnet', url: 'https://testnet.aeternity.io/api?oas3'},
+        ]
     }
 
     SwaggerUIBundle(config);
-}
-
-// cannot call async functions in global context
-load();
+})();
